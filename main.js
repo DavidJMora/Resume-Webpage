0542f709ac8b873039357d83497ec8df74bed1eb
David Mora <David.mora@codeimmersives.com>
2019-04-08 16:03:19 -0400

    erased all data in HTML pertaining to myself and placed it in an object within a newly defined main.js page

---
+++ main.js
@@ -0,0 +1,25 @@
+const profileInfo = {
+    signature: 'I just work here',
+    name: 'DAVID MORA',
+    traits: ['Flexible.', 'Quick Learner.', 'Bottomless Pit.', 'Magnet for Accidents.'],
+    qualifications: ['Web Developer', 'Veteran', 'Brown Guy', 'PLayer 1', 'Perfect Height'],
+    bio: "Once upon a time I got into a fight with my pet snake. Within seconds it was obvious I would succumb to its speed and versatility. Then I was given a stroke of luck and the Koolaid Man bursted through my wall and a piece of random debris knocked the snake out. I quickly tied it into a pretzel and laid claim to my domain. Afterwards, I made a business model on the supply and demand for pretzel fried snakes. Unfortunately none such market existed and so now I have begun my quest on manipulating the world's population into liking my food. Wish me luck.",
+    profilePic: './images/profile.jpeg',
+    footer: '&#169;Mora 2019. All rights reserved.',
+}
+
+const buttons = {
+    
+}
+
+
+
+
+
+
+window.onload = init;
+
+function init() {
+
+}
+
