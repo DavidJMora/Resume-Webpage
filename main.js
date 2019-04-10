68e16b9416d811f51bee35109f62f54f421e7de5
David Mora <David.mora@codeimmersives.com>
2019-04-10 12:20:18 -0400

    added code to function for populating the sidebar to add footer text

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.onload = init;
 function init() {
     populateNavBar();
     populateSideBar();
+    populateMainText();
 
     const home = document.querySelector('#home-button');
     home.addEventListener('click', homeNav);
@@ -34,5 +35,19 @@ function populateSideBar() {
     const name = document.querySelector('#sidebar-header');
     name.appendChild(header);
 
+    const sidebarText = profileInfo.qualifications;
+    sidebarText.forEach(function createLi(words) {
+        const sidebar = document.createElement('li');
+        sidebar.innerText = words;
+        document.querySelector('.sidebar-list').appendChild(sidebar);
+    });
+
+    const footer = document.createElement('summary');
+    footer.innerText = profileInfo.footer;
+    const footer2 = document.querySelector('#sidebar-footer');
+    footer2.appendChild(footer);
 }
 
+function populateMainText() {
+
+}
